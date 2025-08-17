f0ec1bd4e8b3c6db4f97f3ba6dee464f94556a5f
Monazil Chowdhury <ayanctg@gmail.com>
2025-08-17 10:47:11 -0500

    optimize space complexity for grid paths 1

--- cses/1638.cpp
+++ cses/1638.cpp
@@ -2,36 +2,33 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int n;
 const int INT_MOD = 1e9 + 7;
 vector<string> grid;
-vector<vector<int>> dp; 
-int find(int x, int y) {
-    if(x == n - 1 && x == y) {
-        return 1;
-    }
-
-    if(x >= n || y >= n || grid[x][y] == '*') return 0;
-
-    int &ret = dp[x][y];
-    if(ret > -1) return ret;
-
-    return ret = (find(x + 1, y) + find(x, y + 1)) % INT_MOD;
-}
 int main() {
 
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
     cin>>n;
-    grid.resize(n);
-    for(int i = 0; i < n; i++) {
-        cin>>grid[i];
+    string grid;
+    cin>>grid;
+    vector<int> dp(n);
+    for(int i = 0; grid[i] && grid[i] == '.'; i++) {
+        dp[i] = 1;
     }
-    if(grid[n - 1][n - 1] == '*' || grid[0][0] == '*') {
-        cout<<0;
-    } else {
-        dp.resize(n, vector<int> (n, -1));
-        cout<<find(0, 0);
+
+    for(int i = 1; i < n; i++) {
+        cin>>grid;
+        if(grid[0] == '*') dp[0] = 0;
+        for(int j = 1; j < n; j++) {
+            if(grid[j] == '.') {
+                dp[j] = dp[j] + dp[j - 1];
+                if(dp[j] > INT_MOD) dp[j] -= INT_MOD;
+            } else {
+                dp[j] = 0;
+            }
+        }
     }
+    cout<<dp[n - 1];
     return 0;
 }
